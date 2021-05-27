7f30847b6f42e1c4374edf328b2691ff80a445bb
MauriceHendrix <52317399+MauriceHendrix@users.noreply.github.com>
2021-05-27 16:28:26 +0100

    Update rebuild.sh

--- appredict-no-emulators/rebuild.sh
+++ appredict-no-emulators/rebuild.sh
@@ -1,1 +1,1 @@
-pushd && cd /home/appredict/Chaste && scons -j `nproc` cl=1 b=GccOpt exe=1 projects/ApPredict/apps/src && popd
+cd /home/appredict/build/Chaste && scons -j `nproc` cl=1 b=GccOpt exe=1 projects/ApPredict/apps/src && cd ~
